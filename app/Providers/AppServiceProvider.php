3c81e38f2bd4bddb06a8302af60d358981179cd0
Abdul Ojulari <abdulkabirojulari@gmail.com>
2024-07-09 14:13:30 -0600

    install passport and create duplicate checker controller

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Laravel\Passport\Passport;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Client Secret hashing
+        Passport::hashClientSecrets();
+
+        // Token life time 
+        Passport::tokensExpireIn(now()->addDays(15));
+        Passport::refreshTokensExpireIn(now()->addDays(30));
+        Passport::personalAccessTokensExpireIn(now()->addMonths(6));
+        
+        // load the key from storage
+        Passport::loadKeysFrom(__DIR__.'/../secrets/oauth');
     }
 }
