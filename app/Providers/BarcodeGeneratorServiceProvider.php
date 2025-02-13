f0fd378d2c8b26da6c5cf4651021503d7fe4ad83
Abdul Ojulari <abdulkabirojulari@gmail.com>
2025-02-13 08:14:21 -0700

    resolving DI issue

--- app/Providers/BarcodeGeneratorServiceProvider.php
+++ app/Providers/BarcodeGeneratorServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use App\Services\BarcodeGeneratorService;
+use App\Services\RedisService;
 
 class BarcodeGeneratorServiceProvider extends ServiceProvider
 {
@@ -12,8 +13,14 @@ class BarcodeGeneratorServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
+        // Make sure RedisService is registered in the container
+        $this->app->singleton(RedisService::class, function ($app) {
+            return new RedisService();
+        });
+
+        // Now you can register BarcodeGeneratorService and inject RedisService
         $this->app->singleton(BarcodeGeneratorService::class, function ($app) {
-            return new BarcodeGeneratorService();
+            return new BarcodeGeneratorService($app->make(RedisService::class));
         });
     }
 
